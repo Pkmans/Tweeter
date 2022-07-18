782ce190c81f8a2171dabbede0c7faa2ec39d0ee
Aaron Lam <aaronlam2k@gmail.com>
2022-07-18 16:10:38 -0700

    Add Semantic UI Menu bar and Pages Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Container } from 'semantic-ui-react'
+
+import 'semantic-ui-css/semantic.min.css'
 import './App.css';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import MenuBar from './components/MenuBar';
 
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <Router>
+      <Container>
+        <MenuBar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+        </Routes>
+      </Container>
+    </Router>
   );
 }
 
